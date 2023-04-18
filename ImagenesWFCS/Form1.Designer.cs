7eca09e1c0991d8852a195fe88f8eced142c1f67
Andres <andresvazquezo1@hotmail.com>
2023-04-17 19:39:04 -0600

    Add project files.

---
+++ ImagenesWFCS/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImagenesWFCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(473, 34);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(139, 56);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 121);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(1058, 520);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1058, 520);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ImagenesWFCS/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagenesWFCS
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog dialog;
+        Form fullScreenForm;
+        string[] imagenes;
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imagenes = Directory.GetFiles(@"C:\Users\andre\OneDrive\Imágenes\Survival Java");  
+            index = 0;
+           
+        }
+
+        public void btnBrowse_Click(object sender, EventArgs e)
+        {
+            dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\Users\andre\OneDrive\Imágenes\Survival Java";
+            if (dialog.ShowDialog() == DialogResult.OK ) 
+            {
+                pictureBox1.Image = new Bitmap(dialog.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Focus();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /*if(e.KeyCode == Keys.A)
+            {
+                Label label;
+                label = new Label();
+                label.BackColor = Color.Red;
+                label.Size = new Size(300, 300);
+                label.Location = new Point(200, 200);
+                this.Controls.Add(label);
+                pictureBox1.SendToBack();
+            }*/
+            
+
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    if (index == null)
+                    {
+                        index = 0;
+                        pictureBox1.Image = Image.FromFile(imagenes[index]);
+                    }
+                    else if (index == 0)
+                    {
+                        index = 10;
+                        pictureBox1.Image = Image.FromFile(imagenes[index]);
+                    }
+                    else 
+                    {
+                        index--;
+                        pictureBox1.Image = Image.FromFile(imagenes[index]);
+                    }
+                 break;
+                case Keys.D:
+                    if (index == null)
+                    {
+                        index = 0;
+                        pictureBox1.Image = Image.FromFile(imagenes[index]);
+                    }
+                    else if (index == 10)
+                    {
+                        index = 0;
+                        pictureBox1.Image = Image.FromFile(imagenes[index]);
+                    }
+                    else 
+                    {
+                        index++;
+                        pictureBox1.Image = Image.FromFile(imagenes[index]);
+                    }
+                    break;
+            }
+                  
+            
+        }
+
+        public void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            fullScreenForm = new Form();
+            fullScreenForm.Name = "fullScreenForm";
+            fullScreenForm.WindowState = FormWindowState.Maximized;
+            fullScreenForm.FormBorderStyle = FormBorderStyle.None;
+            fullScreenForm.BackColor = Color.Black;
+            fullScreenForm.KeyPreview = true;
+            fullScreenForm.KeyDown += FullScreenForm_KeyDown;
+
+            PictureBox fullScreenPictureBox = new PictureBox();
+            fullScreenPictureBox.Dock = DockStyle.Fill;
+            fullScreenPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            fullScreenPictureBox.Image = pictureBox1.Image;
+
+            fullScreenForm.Controls.Add(fullScreenPictureBox);
+
+            fullScreenForm.ShowDialog();
+        }
+
+        private void FullScreenForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                fullScreenForm.Close();
+            }
+        }
+    }
+}
